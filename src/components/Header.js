a7060af7b4b02d96bf17060c40a2b88263bb58cd
tjallen27 <tjallen91@hotmail.co.uk>
2018-08-17 12:18:16 +0100

    Readme update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render(){
     return (
       <div className="header-container">
-      <h1><span class="outline">Blackout</span> Beats</h1>
+      <h1><span class="outline">Blackout</span> Music</h1>
       <span className="header-underline"></span>
       <h2>Royalty Free. Straight to your inbox.</h2>
       </div>
